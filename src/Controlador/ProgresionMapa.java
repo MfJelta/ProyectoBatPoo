0cfd830d40f23a1a3cc2e07a4524fc195f689523
artor <artor@Oscar>
2025-11-10 23:13:49 -0300

    Version 0.1.2.1

---
+++ src/Controlador/ProgresionMapa.java
@@ -0,0 +1,29 @@
+
+package Controlador;
+
+/**
+ *
+ * @author artor
+ */
+public class ProgresionMapa {
+    private int pisoActual;
+
+    public ProgresionMapa() {
+    }
+
+    public ProgresionMapa(int pisoActual) {
+        this.pisoActual = pisoActual;
+    }
+
+   
+    
+    public int getPisoActual() {
+        return pisoActual;
+    }
+
+    public void setPisoActual(int pisoActual) {
+        this.pisoActual = pisoActual;
+    }
+    
+   
+}
