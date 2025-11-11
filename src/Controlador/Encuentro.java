e93caa9f16da8dd99f8fa1cd1651f53879621be6
artor <artor@Oscar>
2025-11-10 23:13:40 -0300

    Version 0.1.2.1

---
+++ src/Controlador/Encuentro.java
@@ -0,0 +1,23 @@
+
+package Controlador;
+
+import Modelo.Villano;
+import java.util.Random;
+
+/**
+ *
+ * @author artor
+ */
+public class Encuentro {
+    private ProgresionMapa piso;
+    private Villano villanoGenerado;
+
+    public Encuentro(ProgresionMapa piso, Villano villanoGenerado) {
+        this.piso = piso;
+        this.villanoGenerado = villanoGenerado;
+    }
+    
+    //private Villano generarVillano(){
+        //System.out.println("POR IMPLEMENTAR");}
+      
+    }
